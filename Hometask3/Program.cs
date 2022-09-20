79a5fa98dc938c513cdbfc5a3bebacc26f82f337
Andrei Savinkov <andrewsavinkov@gmail.com>
2022-09-20 10:40:28 +0500

    написал код

--- Hometask3/Program.cs
+++ Hometask3/Program.cs
@@ -3,3 +3,26 @@
 7 -> да
 1 -> нет
 */
+string[] CreateWeekArray(string[] week)
+{
+  string[] result = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+  return result;
+}
+
+void IsWeekend ()
+{
+    string[] week = new String[7];
+    string[] newWeek=CreateWeekArray(week);
+    Console.WriteLine("Введите номер дня недели (1- пн, 2- вт, 3- ср, 4- чт, 5-пт, 6-сб, 7-вс): ");
+    int dayNumber = int.Parse(Console.ReadLine());
+    if ((dayNumber>7) || (dayNumber<1))
+    Console.WriteLine("Придерживайтесь заданных числовых рамок!");
+    else if ((dayNumber==6)||(dayNumber==7))
+        Console.WriteLine($"{newWeek[dayNumber-1]} - это Выходной");
+        else
+        Console.WriteLine($"{newWeek[dayNumber-1]} - это Рабочий день");
+
+}
+
+IsWeekend();
+
